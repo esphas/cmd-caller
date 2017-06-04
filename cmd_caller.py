459a78a2761a2c2495d204da0b355530c9a064a5
Esphas <esphas@hotmail.com>
2017-06-04 16:04:37 +0800

    reconstructed

--- cmd_caller.py
+++ cmd_caller.py
@@ -5,74 +5,80 @@ import subprocess
 import sublime
 import sublime_plugin
 
-CmdCallerSettings = 'cmd_caller.sublime-settings'
-Prefix            = '[CmdCaller]'
-BufferUnsaved     = 'Current buffer has not been saved, will use default PWD.'
-InvalidCommand    = 'Invalid argument: cmd should be string or array of strings.'
-DefaultPWD        = '.'
-
 # append text to view
 class AppendTextCommand(sublime_plugin.TextCommand):
   def run(self, edit, text):
     self.view.insert(edit, self.view.size(), text)
 
 # execute one line of command
-class CmdCallerCommand(sublime_plugin.WindowCommand):
+class CmdCaller(sublime_plugin.WindowCommand):
+  Prefix            = '[CmdCaller]'
+  BufferUnsaved     = 'Current buffer has not been saved, will use default PWD'
+  InvalidCommand    = 'Invalid argument: cmd should be a string or an array'
+  CannotExec        = 'Cannot execute command'
+  DefaultPWD        = '.'
+  SettingsFile      = 'cmd_caller.sublime-settings'
 
   # display output message
   def output(self, message):
-    message = Prefix + ' ' + message + '\n'
+    message = self.Prefix + ' ' + message + '\n'
     panel = self.window.find_output_panel('cmd-caller')
     if not panel:
       panel = self.window.create_output_panel('cmd-caller')
     panel.run_command('append_text', {'text': message})
     self.window.run_command('show_panel', {'panel': 'output.cmd-caller'})
 
+  def error(self, message, cmd):
+    self.output(message + ':\n\t' + cmd)
+
+  # load settings
+  def settings(self):
+    return sublime.load_settings(self.SettingsFile)
+
   def run(self, cmd):
     # get directory of current view, if it exists
     view = self.window.active_view()
     if not view.file_name():
       self.output(BufferUnsaved)
-      pwd = DefaultPWD
+      pwd = self.DefaultPWD
     else:
       pwd = os.path.dirname(view.file_name())
     # cmd: [str] and [list of str]
     if isinstance(cmd, list):
-      try:
-        cmd = ' '.join(cmd)
-      except TypeError as e:
-        self.output(InvalidCommand)
-        return
+      cmd = ' '.join(str(c) for c in cmd)
     elif not isinstance(cmd, str):
-      self.output(InvalidCommand)
+      self.error(self.InvalidCommand, cmd)
       return
     # replace variables
     cmd = cmd.replace('%V', pwd)
     # exec
-    subprocess.Popen(cmd)
+    try:
+      subprocess.Popen(cmd)
+    except FileNotFoundError:
+      self.error(self.CannotExec, cmd)
 
 
 # run specific command
-class CmdCallerSpecCommand(sublime_plugin.WindowCommand):
+class CmdCallerSpec(CmdCaller):
   def run(self, app):
-    cmd = sublime.load_settings(CmdCallerSettings).get('apps')[app]['cmd']
-    self.window.run_command('cmd_caller', {'cmd': cmd})
+    cmd = self.settings().get('apps')[app]['cmd']
+    super().run(cmd)
 
 
 # run default command
-class CmdCallerDefaultCommand(sublime_plugin.WindowCommand):
+class CmdCallerDefaultCommand(CmdCallerSpec):
   def run(self):
-    dft = sublime.load_settings(CmdCallerSettings).get('default')
-    self.window.run_command('cmd_caller_spec', {'app': dft})
+    dft = self.settings().get('default')
+    super().run(dft)
 
 
 # run selected command
-class CmdCallerListCommand(sublime_plugin.WindowCommand):
+class CmdCallerListCommand(CmdCallerSpec):
   def run(self):
-    items = list(sublime.load_settings(CmdCallerSettings).get('apps').keys())
+    items = list(item['name'] for item in self.settings().get('apps').values())
     self.window.show_quick_panel(items, lambda idx: self.on_select(items, idx))
 
   def on_select(self, items, idx):
     if idx < 0:
       return
-    self.window.run_command('cmd_caller_spec', {'app': items[idx]})
+    super().run(items[idx])
