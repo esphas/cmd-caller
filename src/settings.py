ff0db6c1dcb1e0a0f5271bf9e5179b2b3f3aa5d8
Esphas <esphas@hotmail.com>
2017-06-06 15:44:31 +0800

    compatibility with linux

--- src/settings.py
+++ src/settings.py
@@ -9,11 +9,10 @@ _package_settings = None
 
 def get(key, default = None):
   global _package_settings
+  _package_settings = sublime.load_settings('cmd_caller.sublime-settings')
   if not _package_settings:
-    _package_settings = sublime.load_settings('cmd_caller.sublime-settings')
-    if not _package_settings:
-      return default
-    _package_settings = _package_settings.get(sublime.platform())
+    return default
+  _package_settings = _package_settings.get(sublime.platform())
   if key in _package_settings:
     return _package_settings[key]
   else:
